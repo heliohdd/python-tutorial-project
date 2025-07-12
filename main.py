3bbcec1ba4b43507e8b4184ff3ba59bebd8a39c5
Helio Dourado <heliohdd@yahoo.com.br>
2025-07-12 20:22:27 -0300

    Added file main.py to commit

---
+++ main.py
@@ -0,0 +1,7 @@
+calculation_to_seconds = 24 * 60  * 60
+name_of_unit = "seconds"
+
+print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
+print(f"35 days are {35 * calculation_to_seconds} {name_of_unit}")
+print(f"50 days are {50 * calculation_to_seconds} {name_of_unit}")
+print(f"110 days are {110 * calculation_to_seconds} {name_of_unit}")
