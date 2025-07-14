77dd0c54509d14a336e665e86ee0b107d8b19df3
Helio Dourado <heliohdd@yahoo.com.br>
2025-07-14 14:45:53 -0300

    Conditional (if / else) and Boolean Data Type

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ name_of_unit = "hours"
 
 
 def days_to_unit(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
-user_input = input("Please enter hours: ")
-user_input_number = int(user_input)
-calculated_value = days_to_unit(user_input_number)
+def validade_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_unit(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered a 0, please enter a valid positive number")
+    else:
+        print('Your input is not a valid number. Don\'t ruim my program.')
 
-print(calculated_value)
+
+user_input = input("Please enter hours: \n")
+
+validade_and_execute()
