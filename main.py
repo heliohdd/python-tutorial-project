46fec31baa330c59dd0a7abb77d88d01c7a55ad3
Helio Dourado <heliohdd@yahoo.com.br>
2025-07-15 14:49:16 -0300

    Dictionary Data Type

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-calculation_to_units = 24
-name_of_unit = "hours"
+def days_to_unit(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} minutes are {num_of_days * 24 * 60} minutes"
+    elif conversion_unit == "seconds":
+        return f"{num_of_days} seconds are {num_of_days * 24 * 60 * 60} seconds"
+    else:
+        return "unsupported unit"
 
 
-def days_to_unit(num_of_days):
-        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-
-def validade_and_execute():
+def validate_and_execute(dictionary):
     try:
-        user_input_number = int(num_of_days_element)
+        user_input_number = int(dictionary["days"])
 
         # We want to do conversion only for positive integers
         if user_input_number > 0:
-            calculated_value = days_to_unit(user_input_number)
+            calculated_value = days_to_unit(user_input_number, dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0:
             print("You entered a 0, please enter a valid positive number")
@@ -20,25 +24,30 @@ def validade_and_execute():
     except ValueError:
         print('Your input is not a valid number. Don\'t ruim my program.')
 
-user_input = ''
+"""user_input = ''
 while user_input != "exit":
-    user_input = input("Hey user, enter number of days as a comma separeted list and I will convert it to hours! \n")
-    list_of_days = user_input.split(", ")
+    user_input = input("Hey user, enter number of days and conversion unit! \n")
+    days_and_unit = user_input.split(":")
+    print(f"You entered: {days_and_unit}")
 
-    print(list_of_days)
-    print(set(list_of_days))
+    days_and_unit_dictionary = {"days": int(days_and_unit[0]), "unit": days_and_unit[1]}
+    print(f"You entered: {days_and_unit_dictionary}")
+    print(f"The type of this input is {type(days_and_unit_dictionary)}")
 
-    print(type(list_of_days))
-    print(type(set(list_of_days)))
-
-    for num_of_days_element in set(list_of_days):
-        validade_and_execute()
+    validate_and_execute(days_and_unit_dictionary)"""
 
 
-print("some text")
-input("enter value")
-set([1, 3, 4])
-int("20")
+message = "enter some value"
+days = 20
+price = 9.99
+valid_number = True
+exit_input = False
+list_of_days = [20, 40, 20, 100]
+list_of_months = ["January", "February", "March"]
+set_of_days = {20, 45, 100}
+days_and_unit = {"days": 10, "unit": "hours"}
 
-"2, 3".split()
-[1, 2, 3].count()
+print(f"Size of message variable: {sys.getsizeof(message)}")
+print(f"Size of days variable: {sys.getsizeof(days)}")
+print(f"Size of price variable: {sys.getsizeof(price)}")
+print(f"Size of valid number variable: {sys.getsizeof(valid_number)}")
