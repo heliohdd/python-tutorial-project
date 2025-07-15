b5437dd0f3f35a57f7279c09147c6c073d2a3850
Helio Dourado <heliohdd@yahoo.com.br>
2025-07-15 08:39:44 -0300

    Comments in Python

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ def days_to_unit(num_of_days):
 def validade_and_execute():
     try:
         user_input_number = int(num_of_days_element)
+
+        # We want to do conversion only for positive integers
         if user_input_number > 0:
             calculated_value = days_to_unit(user_input_number)
             print(calculated_value)
-        elif user_input_number == 0:
+        """elif user_input_number == 0:
             print("You entered a 0, please enter a valid positive number")
         else:
-            print("You entered a negative number, please enter a valid positive number")
+            print("You entered a negative number, please enter a valid positive number")"""
     except ValueError:
         print('Your input is not a valid number. Don\'t ruim my program.')
 
